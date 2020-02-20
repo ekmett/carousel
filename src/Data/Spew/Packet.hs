d5e59900c2349e861f2db7031bc2da6e340b761f
Edward Kmett <ekmett@gmail.com>
2020-02-20 02:10:37 -0800

    more cleanup

--- src/Data/Spew/Packet.hs
+++ src/Data/Spew/Packet.hs
@@ -9,17 +9,6 @@ module Data.Spew.Packet
 import Data.ByteString
 import Data.Serialize
 
--- filename.xxx.nnn.spew, metadata gets written at the end of spewdata
--- xxx = # of datashards
--- nnn = filesize in bytes
--- at the end after we do an in place fix we can truncate
---
--- filelen meta = cdiv filesize (codecPayloadSize * dataShards) * dataShards bytes
--- each one contains a digit for if we've loaded that chunk and digit the hash
---
--- round up file size to 64k and divide by 1k
--- this many bytes
-
 data Packet = Packet
   { fileSize :: {-# UNPACK #-} !Int
   , chunk    :: {-# UNPACK #-} !Int
@@ -27,7 +16,7 @@ data Packet = Packet
   , shards   :: {-# UNPACK #-} !Int
   , payload  :: {-# UNPACK #-} !ByteString
   , fileName :: !ByteString
-  } -- deriving Show
+  }
 
 instance Show Packet where
   showsPrec d Packet{..} = showParen (d > 10) $
@@ -35,16 +24,8 @@ instance Show Packet where
           . showChar ' ' . showsPrec 11 chunk
           . showChar ' ' . showsPrec 11 shard
           . showChar ' ' . showsPrec 11 shards
-          . showChar ' ' . showsPrec 11 (fst $ spanEnd (==0) payload) -- for sanity, we don't Read
+          . showChar ' ' . showsPrec 11 (fst $ spanEnd (==0) payload) -- for readability
           . showChar ' ' . showsPrec 11 fileName
-
-putBS16 :: Putter ByteString
-putBS16 = putNested (putWord16be . fromIntegral) . putByteString
-
-getBS16 :: Get ByteString
-getBS16 = do
-  size <- fromIntegral <$> getWord16be
-  getByteString size
 
 instance Serialize Packet where
   put Packet{..} = do
@@ -52,6 +33,7 @@ instance Serialize Packet where
     putWord64be $ fromIntegral chunk
     putWord8 $ fromIntegral shard
     putWord8 $ fromIntegral shards
+    let putBS16 = putNested (putWord16be . fromIntegral) . putByteString
     putBS16 payload
     putBS16 fileName
   get = do
@@ -59,6 +41,7 @@ instance Serialize Packet where
     chunk <- fromIntegral <$> getWord64be
     shard <- fromIntegral <$> getWord8
     shards <- fromIntegral <$> getWord8
+    let getBS16 = getWord16be >>= getByteString . fromIntegral
     payload <- getBS16
     fileName <- getBS16
     pure Packet{..}
